327d5c4679a137224d1af48e39c7c38819744ccb
yvetteveve <yvetteumubyeyi2@gmail.com>
2019-03-20 11:53:48 +0200

    i change the name of project in content of django setteings module

--- photography/wsgi.py
+++ photography/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photography.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery.settings")
 
 application = get_wsgi_application()
+
+
